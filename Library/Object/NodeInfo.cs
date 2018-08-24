75167d1bd12257532e8ffd07490d7be1ad4e5b3f
trongtin <trongtin@TRONGTINH>
2018-08-24 09:44:38 +0700

    Update

---
+++ Library/Object/NodeInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library.Object
+{
+    public class NodeInfo
+    {
+		public string Id { get; set; }
+		DateTime ngayTao { get; set; }
+		DateTime ngaySua { get; set; }
+    }
+}
