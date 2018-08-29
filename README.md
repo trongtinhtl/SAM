14660c49374ff0702935b57e43273a0ae229f0df
DESKTOP-MUIOR8U\TOLO <trongtinhtl@gmail.com>
2018-08-30 01:04:55 +0700

    + Clean Project

--- README.md
+++
@@ -1,2 +0,0 @@
-# SAM
-Scientific Articles Manarger
--- WACLIM/Assets/js/Ajax/TestAjax.js
+++
@@ -1,9 +0,0 @@
-﻿var TestAjax = {};
-
-TestAjax.TestThanhVien = (callback) => {
-	GLOBAL.callAjax('TestAjax/TestThanhVien',null, callback);
-}
-
-TestAjax.TestBanTin = (callback) => {
-	GLOBAL.callAjax('TestAjax/TestBanTin', null , callback);
-}
